8e7bfe5f48d8b08f7969b2afef85a961b2382465
sionyeom <duatldhs5555@gmail.com>
2022-10-17 16:03:29 +0900

    feat: add pullpage.js library & apply

--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -1,5 +1,41 @@
 import React from "react";
 
+// fullpage library 설치
+import "fullpage.js/vendors/scrolloverflow";
+import "fullpage.js";
+import "fullpage.js/dist/jquery.fullpage.min.css";
+import $ from "jquery";
+// scss import
+import "./landingPage.scss";
+
 export const LandingPage = () => {
-  return <div>LandingPage</div>;
+  $(() => {
+    // 여기서 에러 발생할 수 있음. 해결방법 하단에 적어놨음.
+    $("#fullpage").fullpage({
+      scrollOverflow: true,
+      navigation: true,
+      scrollHorizontally: true,
+      showActiveTooltip: true,
+      scrollingSpeed: 800,
+      fitToSection: true,
+      fitToSectionDelay: 1200,
+      // 화면별 전환
+      afterLoad: function(index, anchorLink) {
+        if (anchorLink === 1) {
+          // 화면별 넣고 싶은 이벤트
+        } else if (anchorLink === 2) {
+          // 화면별 넣고 싶은 이벤트
+        } else if (anchorLink === 3) {
+          // 화면별 넣고 싶은 이벤트
+        }
+      }
+    });
+  });
+
+  return (
+    <div id="fullpage">
+      <div className="section">first page</div>
+      <div className="section">second page</div>
+    </div>
+  );
 };
