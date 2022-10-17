cf8c4cf0eb1f031ab68f622880a8112965520388
sionyeom <duatldhs5555@gmail.com>
2022-10-17 17:04:09 +0900

    feat: 랜딩 페이지 백그라운드 이미지 지정

---
+++ client/src/components/LandingPage/FirstPage/FirstPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./firstPage.scss";
+
+const FirstPage = () => {
+  return (
+    <div className="first_page_wrapper">
+      <div />
+    </div>
+  );
+};
+
+export default FirstPage;
---
+++ client/src/components/LandingPage/SecondPage/SecondPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./secondPage.scss";
+
+const SecondPage = () => {
+  return <div className="second_page_wrapper">SecondPage</div>;
+};
+
+export default SecondPage;
