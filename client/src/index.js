49b1af601d3769c74e0000c14264a8c355a6093d
sionyeom <duatldhs5555@gmail.com>
2022-10-18 12:40:00 +0900

    fix: 랜딩페이지 레이아웃 수정

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import "./styles/common.scss"
-import reportWebVitals from './reportWebVitals';
-import { LandingPage } from './pages/LandingPage/LandingPage';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "./styles/common.scss";
+import reportWebVitals from "./reportWebVitals";
+import { LandingPage } from "./pages/LandingPage/LandingPage";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
     <LandingPage />
-    <Input_pw />
-    <Input_id />
-    <Sign_in />
-    <Login_Btn />
-    <Find_txt />
   </>
 );
 
