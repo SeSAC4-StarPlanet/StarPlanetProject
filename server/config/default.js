ccb768fe6f1154bbe93dfd016a8e1667120bb369
idledaydreamer <idledaydreamer@korea.ac.kr>
2022-10-20 18:24:47 +0900

    feat: DB, Redis 연결 작업

--- server/config/default.js
+++ server/config/default.js
@@ -1,7 +1,14 @@
 module.exports = {
-    db: {
-        mongoUrl: "mongodb+srv://test:<password>@starplanet.i5qsztg.mongodb.net/?retryWrites=true&w=majority"
-    },
     port: 8000,
     client: "", //[프로젝트]-[실행 URL과 포트]에서 설정하신 client domain을 넣어주세요!
+    db: {
+        MONGO_URI: "mongodb+srv://test:12340987@starplanet.i5qsztg.mongodb.net/?retryWrites=true&w=majority"
+    },
+    redis: {
+        REDIS_HOST: 'redis-15601.c15.us-east-1-4.ec2.cloud.redislabs.com',
+        REDIS_PORT: 15601,
+        REDIS_USERNAME: 'default',
+        REDIS_PASSWORD: 'grmoQtkOvvIZmNaeXQ8Krn3BTRAN5vmp'
+    },
+    COOKIE_SECRET: 'apple',
 }
