3db60a2ce8557d6b293cbd7ad03314e688807e82
sionyeom <duatldhs5555@gmail.com>
2022-10-20 16:37:51 +0900

    feat: 테스트 코드 작성

--- client/src/components/Test/SionTest/SionTest.js
+++ client/src/components/Test/SionTest/SionTest.js
@@ -3,23 +3,19 @@ import "./sionTest.scss";
 
 import StarMap from "../../Common/StarMap/StarMap";
 import StarWarp from "../../Common/StarWarp/StarWarp";
-import Categorybar from "../../Common/Diary/Categorybar/Categorybar";
-import Diary_content from "../../Common/Diary/Diary_content/Diary_content";
+// import Categorybar from "../../Common/Diary/Categorybar/Categorybar";
+// import Diary_content from "../../Common/Diary/Diary_content/Diary_content";
 import Diary_main from "../../Common/Diary/Diary_main";
-
+import Header from "../../Common/Diary/Header/Header";
+import SearchBar from "../../Common/Diary/SearchBar/SearchBar";
 const SionTest = () => {
   return (
-    <div>
+    <>
+      <Header />
+      {/* <SearchBar /> */}
       <StarMap />
-      {/* <Diary_content
-        title={"title"}
-        content1={"content1"}
-        content2={"content2"}
-        content3={"content3"}
-        content4={"content4"}
-      /> */}
       <Diary_main planetTitle={"planetTitle"} />
-    </div>
+    </>
   );
 };
 
