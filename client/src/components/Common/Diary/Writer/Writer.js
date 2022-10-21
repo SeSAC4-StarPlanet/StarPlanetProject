ae2f3b149a0b189b0e6edb0cbfc33542745ca10d
sionyeom <duatldhs5555@gmail.com>
2022-10-21 21:27:18 +0900

    feat: 글쓰기 사이트 수정

--- client/src/components/Common/Diary/Writer/Writer.js
+++ client/src/components/Common/Diary/Writer/Writer.js
@@ -13,52 +13,54 @@ import { FaPlay } from "react-icons/fa";
 const Writer = () => {
   const CssTextField = styled(TextField)({
     "& label.Mui-focused": {
-      color: "#3c52b2"
+      color: "#3c52b2",
     },
     "& .MuiInput-underline:after": {
-      borderBottomColor: "#3c52b2"
+      borderBottomColor: "#3c52b2",
     },
     "& .MuiOutlinedInput-root": {
       "& fieldset": {
-        borderColor: "#3c52b2"
+        borderColor: "#3c52b2",
       },
       "&:hover fieldset": {
-        borderColor: "#3c52b2"
+        borderColor: "#3c52b2",
       },
       "&.Mui-focused fieldset": {
-        borderColor: "#3c52b2"
-      }
-    }
+        borderColor: "#3c52b2",
+      },
+    },
   });
 
   return (
-    <div className="dairyWriterWrapper">
-      <div className="dairyWriterArrow" />
-      <div className="dairyWriterTitleContainer">
-        <CssTextField
-          sx={{ width: "900px", color: "#3c52b2" }}
-          rows={1}
-          rowsMax={1}
-          size="small"
-          label="제목"
-          variant="standard"
-        />
-        <Button
-          sx={{
-            backgroundColor: "#0D0D7E",
-            "&:hover": {
-              backgroundColor: "#fff",
-              color: "#3c52b2"
-            }
-          }}
-          variant="contained"
-          endIcon={<FaPlay />}
-        >
-          작성 완료
-        </Button>
-      </div>
-      <div className="dairyWriterEditorContainer">
-        <Editor />
+    <div className="dairyWriterBackWrapper">
+      <div className="dairyWriterWrapper">
+        <div className="dairyWriterArrow" />
+        <div className="dairyWriterTitleContainer">
+          <CssTextField
+            sx={{ width: "900px", color: "#3c52b2" }}
+            rows={1}
+            rowsMax={1}
+            size="small"
+            label="제목"
+            variant="standard"
+          />
+          <Button
+            sx={{
+              backgroundColor: "#0D0D7E",
+              "&:hover": {
+                backgroundColor: "#fff",
+                color: "#3c52b2",
+              },
+            }}
+            variant="contained"
+            endIcon={<FaPlay />}
+          >
+            작성 완료
+          </Button>
+        </div>
+        <div className="dairyWriterEditorContainer">
+          <Editor />
+        </div>
       </div>
     </div>
   );
