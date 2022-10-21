e3095be5762f510c32aba6b072f5ba9d94a2ec98
sionyeom <duatldhs5555@gmail.com>
2022-10-21 23:41:44 +0900

    feat: 다이어리 게시판 & 작성 페이지 작성

--- client/src/components/Common/Diary/Diary_main.js
+++ client/src/components/Common/Diary/Diary_main.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Diary_main.scss";
 import Diary_content from "../../Common/Diary/Diary_content/Diary_content";
 import Planet_name from "../../Common/Diary/Planet_name/Planet_name";
@@ -8,92 +8,67 @@ import Selector from "./Selector/Selector";
 import IconButton from "@mui/material/IconButton";
 import { FaPen } from "react-icons/fa";
 
+import usePagination from "./Pagination";
+import Pagination from "@mui/material/Pagination";
+
+// MOCK 데이터
+import DATA from "./data.js";
+
 const Diary_main = ({ planetTitle }) => {
+  let [page, setPage] = useState(1);
+  const PER_PAGE = 6;
+
+  const count = Math.ceil(DATA.length / PER_PAGE);
+  const _DATA = usePagination(DATA, PER_PAGE);
+
+  const handleChange = (e, p) => {
+    setPage(p);
+    _DATA.jump(p);
+  };
+
   return (
-    <div className="Main_wrapper">
-      <div className="mainArrow" />
-      <div className="Main_container_top">
-        <div className="Planet_name_box">
-          <Planet_name title={planetTitle} />
-        </div>
-        <div className="Modify_title_box">
-          <Planet_name title={"2022"} />
-          <IconButton>
-            <FaPen />
-          </IconButton>
-        </div>
-      </div>
-      <div className="Main_container">
-        <div className="Categorybar_box">
-          <div>
-            <Category />
+    <div className="mainBackWrapper">
+      <div className="Main_wrapper">
+        <div className="mainArrow" />
+        <div className="Main_container_top">
+          <div className="Planet_name_box">
+            <Planet_name title={planetTitle} />
           </div>
-          <div>
-            <MemberBox />
+          <div className="Modify_title_box">
+            <Planet_name title={"2022"} />
+            <IconButton>
+              <FaPen />
+            </IconButton>
           </div>
         </div>
-        <div className="Main_box">
-          <div className="Diary_selector_box">
-            <Selector />
-          </div>
-          <div className="Diary_main_box">
+        <div className="Main_container">
+          <div className="Categorybar_box">
             <div>
-              <Diary_content
-                title={"오늘 할 일"}
-                writer={"염시온"}
-                date={"2022-01-01"}
-                content1={"코딩공부하기"}
-                content2={"코딩공부하기"}
-                content3={"코딩공부하기"}
-                content4={"코딩공부하기"}
-              />
-              <Diary_content
-                title={"오늘 할 일"}
-                writer={"염시온"}
-                date={"2022-01-01"}
-                content1={"코딩공부하기"}
-                content2={"코딩공부하기"}
-                content3={"코딩공부하기"}
-                content4={"코딩공부하기"}
-              />
-              <Diary_content
-                title={"오늘 할 일"}
-                writer={"염시온"}
-                date={"2022-01-01"}
-                content1={"코딩공부하기"}
-                content2={"코딩공부하기"}
-                content3={"코딩공부하기"}
-                content4={"코딩공부하기"}
-              />
+              <Category />
             </div>
             <div>
-              <Diary_content
-                title={"오늘 할 일"}
-                writer={"염시온"}
-                date={"2022-01-01"}
-                content1={"코딩공부하기"}
-                content2={"코딩공부하기"}
-                content3={"코딩공부하기"}
-                content4={"코딩공부하기"}
-              />
-              <Diary_content
-                title={"오늘 할 일"}
-                writer={"염시온"}
-                date={"2022-01-01"}
-                content1={"코딩공부하기"}
-                content2={"코딩공부하기"}
-                content3={"코딩공부하기"}
-                content4={"코딩공부하기"}
-              />
-              <Diary_content
-                title={"오늘 할 일"}
-                writer={"염시온"}
-                date={"2022-01-01"}
-                content1={"코딩공부하기"}
-                content2={"코딩공부하기"}
-                content3={"코딩공부하기"}
-                content4={"코딩공부하기"}
-              />
+              <MemberBox />
+            </div>
+          </div>
+          <div className="Main_box">
+            <div className="Diary_selector_box">
+              <Selector />
+            </div>
+            <div className="Diary_main_box">
+              {_DATA.currentData().map((e) => {
+                console.log(e);
+                return <Diary_content title={e.title} />;
+              })}
+              <div className="dairyPaginationWrapper">
+                <Pagination
+                  count={count}
+                  size="large"
+                  page={page}
+                  color="secondary"
+                  shape="rounded"
+                  onChange={handleChange}
+                />
+              </div>
             </div>
           </div>
         </div>
--- client/src/pages/Diary/Main/DiaryMain.js
+++ client/src/pages/Diary/Main/DiaryMain.js
@@ -1,40 +1,16 @@
 import React, { useState } from "react";
 import "./diaryMain.scss";
-import usePagination from "./Pagination";
-import Pagination from "@mui/material/Pagination";
 
-// MOCK 데이터
-import DATA from "./data.js";
-// 컴포넌트
-import Diary_content from "../../../components/Common/Diary/Diary_content/Diary_content";
+import StarMap from "../../../components/Common/StarMap/StarMap";
+import Header from "../../../components/Common/Diary/Header/Header";
+import Diary_main from "../../../components/Common/Diary/Diary_main";
 
 const DiaryMain = ({}) => {
-  let [page, setPage] = useState(1);
-  const PER_PAGE = 6;
-
-  const count = Math.ceil(DATA.length / PER_PAGE);
-  const _DATA = usePagination(DATA, PER_PAGE);
-
-  const handleChange = (e, p) => {
-    setPage(p);
-    _DATA.jump(p);
-  };
-
-  // console.log(DATA);
   return (
-    <div>
-      {_DATA.currentData().map((e) => {
-        console.log(e);
-        return <Diary_content title={e.title} />;
-      })}
-      <Pagination
-        count={count}
-        size="large"
-        page={page}
-        color="secondary"
-        shape="rounded"
-        onChange={handleChange}
-      />
+    <div className="back">
+      <StarMap />
+      <Header />
+      <Diary_main />
     </div>
   );
 };
