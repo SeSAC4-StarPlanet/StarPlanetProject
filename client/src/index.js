886b3bb43f1096097e5b20b40d49fde6d9befba3
sionyeom <duatldhs5555@gmail.com>
2022-10-21 17:07:20 +0900

    fix: index 수정

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
-import React ,{useEffect}from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./styles/common.scss";
@@ -7,13 +7,13 @@ import axios from 'axios';
 import Router from "./components/Router/Router";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const callApi = async()=> {
-  axios.get('/api').then((res)=> console.log(res.data.test));
-}
+// const callApi = async()=> {
+//   axios.get('/api').then((res)=> console.log(res.data.test));
+// }
 
-useEffect(()=>{
-  callApi();
-}, []);
+// useEffect(()=>{
+//   callApi();
+// }, []);
 
 root.render(
   <>
