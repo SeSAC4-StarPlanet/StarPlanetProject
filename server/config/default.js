5bc6e634b1c34471bdf3a5ed8b1995fc700b1c64
idledaydreamer <idledaydreamer@korea.ac.kr>
2022-10-31 15:49:32 +0900

    dbConfig

--- server/config/default.js
+++ server/config/default.js
@@ -3,7 +3,7 @@ module.exports = {
   client: "http://localhost:3000", //[프로젝트]-[실행 URL과 포트]에서 설정하신 client domain을 넣어주세요!
   db: {
     MONGO_URI:
-      "mongodb+srv://starplanet:12340987@starplanet.i5qsztg.mongodb.net/?retryWrites=true&w=majority",
+      "mongodb+srv://starplanet:f8qz9bLRfdEvT2S5@starplanet.i5qsztg.mongodb.net/?retryWrites=true&w=majority",
   },
   redis: {
     REDIS_HOST: "redis-15601.c15.us-east-1-4.ec2.cloud.redislabs.com",
