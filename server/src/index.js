ed030e9450b068cb241188cb72a5d5f8048a8b2d
idledaydreamer <idledaydreamer@korea.ac.kr>
2022-11-02 14:58:38 +0900

    starplanet: release-1.0.0

--- server/src/index.js
+++ server/src/index.js
@@ -60,10 +60,11 @@ app.use(passport.session());
 
 
 //* routes */
-app.use("/api", require("./routes/api"));
-// app.get('*', function (req, res) {
-//     res.sendFile(path.join(__dirname, '/client/build/index.html'));
-// });
+// app.use("/api", require("./routes/api"));
+app.use(express.static(path.join(__dirname, '../../client/build')));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../../client/build', "index.html"));
+});
 
 
 app.use((req, res, next) => {
@@ -76,16 +77,6 @@ app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.send({ msg: err.message })
-});
 
 
 
